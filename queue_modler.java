9a2a5dff35f2bdc3505cb55337e0bb471743181f
george_IVabil <45440666+aion1@users.noreply.github.com>
2019-03-10 00:54:06 +0200

    queue modular
    queuing module is responsible for in memory
    data caching, until data store has actually committed the data to persistence storage in a database or file.

---
+++ queue_modler.java
@@ -0,0 +1,46 @@
+package jReto;
+import java.util.*;
+import java.io.*;
+public class queue_modler {
+    private List queue = new LinkedList();
+    private int limit = 30;
+
+    public queue_modler(int limit) {
+        this.limit=limit;
+    }
+    public synchronized void enqueue(FakeDataGenerator item) throws InterruptedException  {
+        while(this.queue.size() == this.limit) {
+
+        appendStrToFile("/home/george/P2P_2", (String) queue.get(queue.size() - 1));
+
+        this.queue.remove(0);}
+        if(this.queue.size() == 0) {
+            notifyAll();
+        }
+        this.queue.add(item.data);
+    }
+    public synchronized Object dequeue() throws InterruptedException{
+        while(this.queue.size() == 0){
+            wait();
+        }
+        if(this.queue.size() == this.limit){
+            notifyAll();
+        }
+
+        return this.queue.remove(0);
+    }
+    public static void appendStrToFile(String fileName, String str)
+    {
+        try {
+
+            // Open given file in append mode.
+            BufferedWriter out = new BufferedWriter(
+            new FileWriter(fileName, true));
+            out.write(str);
+            out.close();
+        }
+        catch (IOException e) {
+            System.out.println("exception occoured" + e);
+        }
+    }
+}
